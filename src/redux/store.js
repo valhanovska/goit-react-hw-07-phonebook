b72bd3fa5b848baf5128733f0345f966732d1497
Valentyna Hanovska <riefenstahl.valli@gmail.com>
2022-10-06 18:15:43 +0300

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,28 +1,29 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { persistedReducer } from './slice';
+// import { persistedReducer } from './slice';
 import filterReducer from './filterSlice';
-import {
-  persistStore,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
+import contactsReducer from './slice';
+// import {
+//   persistStore,
+//   FLUSH,
+//   REHYDRATE,
+//   PAUSE,
+//   PERSIST,
+//   PURGE,
+//   REGISTER,
+// } from 'redux-persist';
 
 export const store = configureStore({
   reducer: {
-    contacts: persistedReducer,
+    contacts: contactsReducer,
     filter: filterReducer,
   },
-  middleware(getDefaultMiddleware) {
-    return getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    });
-  },
+  // middleware(getDefaultMiddleware) {
+  //   return getDefaultMiddleware({
+  //     serializableCheck: {
+  //       ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+  //     },
+  //   });
+  // },
 });
 
-export const persistor = persistStore(store);
+// export const persistor = persistStore(store);
