cd09eb9ce799613cfd839e0d4ffa4f9d9222ee52
Valentyna Hanovska <riefenstahl.valli@gmail.com>
2022-10-06 18:15:26 +0300

    Update ContactList.jsx

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,10 +1,20 @@
 import ContactItem from '../ContactItem';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { getContacts, getFilter } from 'redux/selectors';
+import { useEffect } from 'react';
+import { fetchContacts } from 'redux/operations';
 
 const ContactList = () => {
   const contacts = useSelector(getContacts);
   const filter = useSelector(getFilter);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (contacts.length === 0) {
+      dispatch(fetchContacts());
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   const lowerCaseContact = contacts.filter(({ name }) =>
     name.toLowerCase().includes(filter.trim())
@@ -14,8 +24,8 @@ const ContactList = () => {
     <>
       {lowerCaseContact.length > 0 && (
         <ul>
-          {lowerCaseContact.map(({ id, name, number }) => (
-            <ContactItem key={id} name={name} number={number} id={id} />
+          {lowerCaseContact.map(({ id, name, phone }) => (
+            <ContactItem key={id} name={name} number={phone} id={id} />
           ))}
         </ul>
       )}
